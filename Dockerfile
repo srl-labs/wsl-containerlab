188e42b7e456c4483ad2d175fb8bae82e1d80fcb
kaelemc <62122480+kaelemc@users.noreply.github.com>
2024-12-01 23:58:28 +1300

    add files

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+from debian:bookworm-slim
+
+RUN apt update -y && apt upgrade -y
+RUN apt install git curl sudo -y && rm -rf /var/lib/apt/lists/*
+
+RUN curl -sL https://containerlab.dev/setup | sudo -E bash -s "all"
+
+COPY --chmod=644 --chown=root:root ./wsl-distribution.conf /etc/wsl-distribution.conf
+COPY --chmod=644 --chown=root:root ./wsl.conf /etc/wsl.conf
+COPY --chmod=755 ./oobe.sh /etc/oobe.sh
+
+
---
+++ oobe.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ue
+
+DEFAULT_GROUPS='adm,cdrom,sudo,dip,plugdev'
+DEFAULT_UID='1000'
+
+echo 'Please create a default UNIX user account. The username does not need to match your Windows username.'
+echo 'For more information visit: https://aka.ms/wslusers'
+
+if getent passwd "$DEFAULT_UID" > /dev/null ; then
+  echo 'User account already exists, skipping creation'
+  exit 0
+fi
+
+while true; do
+
+  # Prompt from the username
+  read -p 'Enter new UNIX username: ' username
+
+  # Create the user
+  if /usr/sbin/adduser --uid "$DEFAULT_UID" --quiet --gecos ''  "$username"; then
+
+    if /usr/sbin/usermod "$username" -aG "$DEFAULT_GROUPS"; then
+      break
+    else
+      /usr/sbin/deluser "$username"
+    fi
+  fi
+done
+
+
