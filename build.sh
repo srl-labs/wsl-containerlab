5c854b5a14c3ef2f68bebcdfc216109631a19d88
flosch62 <schwarz.flori.88@gmail.com>
2024-12-12 16:15:23 +0100

    +x build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@ docker rm wsl_export
 
 docker build . --tag ghcr.io/kaelemc/clab-wsl-debian
 
-mkdir /mnt/c/temp/
-mv /mnt/c/temp/clab.wsl /mnt/c/temp/clab.wsl.old
+mkdir /mnt/c/Temp/
+mv /mnt/c/Temp/clab.wsl /mnt/c/Temp/clab.wsl.old
 
 docker run -t --name wsl_export ghcr.io/kaelemc/clab-wsl-debian ls /
 
 echo "Copying..."
-docker export wsl_export > /mnt/c/temp/clab.wsl
+docker export wsl_export > /mnt/c/Temp/clab.wsl
 
 echo "Cleaning up..."
 docker rm wsl_export
