476951dbc1303caed9d22d7729eed3ed4906f095
Florian Schwarz <schwarz.flori.88@gmail.com>
2025-02-03 12:37:31 +0100

    zsh completion

--- zsh/install-zsh-plugins.sh
+++ zsh/install-zsh-plugins.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env bash
-# atuin
-# bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
+# Install atuin
 curl -LsSf https://github.com/atuinsh/atuin/releases/download/v18.3.0/atuin-installer.sh | sh
 
-# theme
-git clone --depth 1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# Install Powerlevel10k theme
+git clone --depth 1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-# zsh-autosuggestions and autocompletions
-git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+# Install zsh-autosuggestions and zsh-autocomplete plugins
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete"
+# Patch zsh-autocomplete to remove unsupported "-P" option:
+sed -i 's/local -P/local/g' "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
 
-# syntax highlighting
-git clone --depth 1 https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
+# Install syntax highlighting plugin
+git clone --depth 1 https://github.com/z-shell/F-Sy-H.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H"
 
-###
-### Shell completions
-###
-
-containerlab completion zsh > _containerlab
-mv _containerlab ~/.oh-my-zsh/custom/plugins/zsh-autocomplete/Completions/_containerlab
-
-# add clab alias to the completions
-sed -i 's/compdef _containerlab containerlab/compdef _containerlab containerlab clab/g' "/home/clab/.oh-my-zsh/custom/plugins/zsh-autocomplete/Completions/_containerlab"
+# Install containerlab completions via the tools completions script:
+bash -c "/tmp/install-tools-completions.sh"
